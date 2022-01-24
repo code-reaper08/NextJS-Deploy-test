9688a17c5b3d9d909107328f7192467ca1128c88
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2022-01-24 16:41:39 +0000

    Deploying to gh-pages from @ code-reaper08/NextJS-Deploy-test@13222ef14025541a9472d397fdbfd879254253ad 🚀

---
+++ _next/static/npYmCLjQosRm7E6PmBCh_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-0cec54d0aa917f1d.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/about":["static/chunks/pages/about-df55184ab11db766.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
